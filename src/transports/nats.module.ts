b959fc81455d0150fdcbac93e21fa7cc367bfdbb
Gonzalo Murgueytio <gmurgueytio@cooprogreso.fin.ec>
2024-07-09 09:23:55 -0500

    ok

---
+++ src/transports/nats.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NATS_SERVICE, envs } from 'src/config';
+
+@Module({
+    imports: [
+        ClientsModule.register([
+
+            { 
+                name: NATS_SERVICE, 
+                //transport: Transport.TCP,
+                transport: Transport.NATS,
+                options: {
+                //host: envs.productsMicroserviceHost,
+                //port: envs.productsMicroservicePort
+                servers: envs.natsServers,
+                }
+            },
+            
+          ]),
+    ],
+    exports: [
+        ClientsModule.register([
+
+            { 
+              name: NATS_SERVICE, 
+              //transport: Transport.TCP,
+              transport: Transport.NATS,
+              options: {
+                //host: envs.productsMicroserviceHost,
+                //port: envs.productsMicroservicePort
+                servers: envs.natsServers,
+              }
+            },
+            
+          ]),
+    ]
+})
+export class NatsModule {}
